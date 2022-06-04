c5e59b278187c80a31a158a3504d6f9b9004bea4
Adeesha97 <adeeshakawshal1997@gmail.com>
2022-06-04 20:28:51 +0530

    first commit

---
+++ src/common/api/MovieApiKey.jsx
@@ -0,0 +1,1 @@
+export const APIKEY = "56ace135";
---
+++ src/components/MovieLisiting/MovieLisiting.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieLisiting = () => {
+  return <div>MovieLisiting</div>;
+};
+
+export default MovieLisiting;
